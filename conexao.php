9ba0dafa9356d8749f9866023fa5756170a848c4
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-06-13 17:10:17 -0300

    criamos os htmls basicos

---
+++ conexao.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Faz uma conexão com o banco de dados Mysql,
+ * na base de dados recuperar-senha.
+ * 
+ * @return retorna uma conexão com a base de dados, ou em caso de falha,
+ * mata a execução e exibe o erro.
+ * 
+ */
+function conectar(){
+    $conexao = mysqli_connect("localhost", "root", "", "recuperar-senha");
+    if ($conexao === false) {
+        echo "Erro ao conectar à base de dados. Nº do erro: " . 
+        mysqli_connect_errno() . ". ".
+        mysqli_connect_errno();
+        die();
+    }
+    return $conexao;
+}
+
+?>
