f2b96e9c3dec863191e3b57b248ba858f49668e6
Luiz Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-07-08 17:04:45 -0300

    tudo

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-
+    <h1>Inicial</h1>
 <body>
     <form action="login.php" method="post">
-        <label>Email: <input type="email" name="email"></label><br>
-        <label>Senha: <input type="password" name="senha"></label><br>
+        <label>Email: <input type="email" name="email"></label><br><br>
+        <label>Senha: <input type="password" name="senha"></label><br><br>
         <input type="submit" value="Login">
     </form><br><br>
     <a href="form-cadastro.php">Cadastrar-se</a><br><br>
--- nova-senha.php
+++ nova-senha.php
@@ -2,7 +2,27 @@
 //verificar o email
 //verificar o tokem
 $email = $_GET['email'];
-$token = $_GET['token']
+$token = $_GET['token'];
 
-
+require_once "conexao.php";
+$conexao = conectar();
+$sql = "SELECT * FROM `recuperar-senha` WHERE email='$email' AND token='$token'";
+$resultado = executarSQL($conexao,$sql);
+$recuperar = mysqli_fetch_assoc($resultado);
+if ($recuperar == null){
+    echo "Email ou token incorreto. Tente fazer um novo pedido de recuperação de senha";
+    die();
+} else {
+    //verificar a validade do pedido (data_criacao)
+    //verificar se o link já foi usado 
+    date_default_timezone_set('America/Sao_Paulo');
+    $agora = new DateTime('now');
+    $data_criacao = DateTime::createFromFormat('Y-m-d H:i:s', $recuperar['data_criacao']);
+    $umDia = DateInterval::createFromDateString('1 day');
+    $dataExpiracao = date_add($data_criacao,$umDia);
+    
+    if ($agora < $dataExpiracao){
+        echo "Funcionou!";
+    }
+}
 ?>
